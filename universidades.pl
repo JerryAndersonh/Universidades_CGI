7d9282ea1042fec7ec60201a5e5aac8c7a6abf3c
Brayan Carlos Auccacusi Conde <bauccacusic@unsa.edu.pe>
2024-11-14 19:39:02 -0500

    Inputs de columna y palabra buscada, busqueda del indice columna, busqueda en cada fila de columna e impresion

--- universidades.pl
+++ universidades.pl
@@ -13,3 +13,21 @@ open(my $fh, '<', $data) or die "No se pudo abrir el archivo '$data' $!";
 
 my $csv = Text::CSV->new({ binary => 1, sep_char => ',' });
 my @columnas = @{ $csv->getline($fh) };
+
+my $columna = $cgi->Param('columna');
+my $termino = $cgi->Param('termino');
+my $index;
+
+for (my $i = 0; $i < @columnas; $i++){
+    if($columnas[$i] eq $columna){
+        $index = $i;
+    }
+}
+
+while (my @fila = $csv->getline($fh)){
+    if ($fila[$index]=~/\Q$termino\E/i){
+        print "<tr>";
+        print "<td>$_</td>" for @fila;
+        print "</tr>";
+    }
+}
