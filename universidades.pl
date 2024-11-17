0be9f20026970045598688979ce8061a0ceb7aa0
Brayan Carlos Auccacusi Conde <bauccacusic@unsa.edu.pe>
2024-11-17 17:38:49 -0500

    Mejora perl, mas use, codigo html

--- universidades.pl
+++ universidades.pl
@@ -2,11 +2,30 @@
 use strict;
 use warnings;
 use CGI;
-use Text::CSV;
+use utf8;
+use open ':std', ':encoding(UTF-8)';
+use Unicode::Normalize;
 
 my $cgi = CGI->new;
-print $cgi->header('text/html; charset=UTF-8');
+$cgi->charset('UTF-8');
+print $cgi->header(-type => 'text/html', -charset => 'UTF-8');
 
+print <<HTML;
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Página de Búsqueda - Universidades licenciadas</title>
+  </head>
+  <body>
+    <div class="site-wrapper">
+      <div class="mytitle">
+        <b>Resultados de la búsqueda</b>
+      </div>
+      <div class="content answer">
+HTML
 
 my $data = 'DataUNI.csv';
 open(my $fh, '<', $data) or die "No se pudo abrir el archivo '$data' $!";
